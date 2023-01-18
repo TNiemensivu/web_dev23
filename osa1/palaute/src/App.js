71cca2075c19971d1388095632fa2ecf46865c7b
Timi Niemensivu <timinie@utu.fi>
2023-01-18 13:13:29 +0200

    Exercises 1.6-1.10

--- osa1/palaute/src/App.js
+++ osa1/palaute/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+    return (
+      <div>
+        <Header/>
+        <Vote handleClick={()=>setGood(good+1)} buttonText={"good"}/>
+        <Vote handleClick={()=>setNeutral(neutral+1)} buttonText={"neutral"}/>
+        <Vote handleClick={()=>setBad(bad+1)} buttonText={"bad"}/>
+        <Statistics good={good} neutral={neutral} bad={bad}/>
+      </div>
+    )
 }
 
-export default App;
+const Header = () =>{
+  return (
+    <h1>give feedback</h1>
+  )
+}
+
+const Vote = (props) =>{
+  return(
+  <button onClick={props.handleClick}>{props.buttonText}</button>
+  )
+}
+
+
+const Statistics = (props) => {
+  const total= props.good+props.neutral+props.bad
+  const totalVal= props.good-props.bad
+  if(total === 0){
+    return (
+      <div>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+  return(
+    <div>
+      <h1>statistics</h1>
+      <StatisticLine text="good" value ={props.good} />
+      <StatisticLine text="neutral" value ={props.neutral} />
+      <StatisticLine text="bad" value ={props.bad} />
+      <StatisticLine text="average" value={totalVal/total}/>
+      <StatisticLine text="positive" value={100*props.good/total}/>
+    </div>
+  )
+}
+
+const StatisticLine = (props) =>{
+  return(
+    <p>{props.text} {props.value}</p>
+  )
+}
+
+export default App

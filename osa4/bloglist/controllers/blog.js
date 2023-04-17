2593f7db206158cdc650eed614afe3907844789d
Timi Niemensivu <timinie@utu.fi>
2023-04-17 12:17:40 +0300

    Osa4

---
+++ osa4/bloglist/controllers/blog.js
@@ -0,0 +1,77 @@
+const blogRouter = require('express').Router()
+const logger = require('../utils/logger')
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
+
+blogRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    response.json(blogs.map(blog => blog.toJSON()))
+})
+
+blogRouter.post('/', async (request, response, next) => {
+    const body = request.body
+
+    const token = request.token
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+
+    const user = await User.findById(decodedToken.id)
+
+    if (!body.likes) {
+        body.likes = 0
+    }
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+        user: user._id
+    })
+
+    try {
+        const savedBlog = await blog.save()
+        logger.info(`added ${blog.title} to the blog list`)
+        randomUser.blogs = randomUser.blogs.concat(savedBlog._id)
+        await randomUser.save()
+        logger.info(`blog linked to user ${randomUser.username}`)
+        response.json(savedBlog.toJSON())
+    } catch(exception) {
+        next(exception)
+    }
+})
+
+blogRouter.delete('/:id', async (request, response, next) => {
+    try {
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()
+    } catch (exception) {
+      next(exception)
+    }
+  })
+
+blogRouter.put('/:id', async (request, response, next) => {
+    const body = request.body
+
+    if (!body.likes) {
+        body.likes = 0
+    }
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+        logger.info(`blog ${blog.title} successfully updated`)
+        response.json(updatedBlog.toJSON())
+    } catch (exception) {
+        next(exception)
+    }
+})
+
+
+module.exports = blogRouter
---
+++ osa4/bloglist/tests/bloglist.test.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose')
+const helper = require('./test_helper')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+
+  const noteObjects = helper.initialBlogs
+    .map(blog => new Blog(blog))
+  const promiseArray = noteObjects.map(blog => blog.save())
+  await Promise.all(promiseArray)
+})
+
+describe('Get blog information', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('there are two blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('the first blog is about React patterns', async () => {
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(r => r.title)
+
+    expect(contents).toContain('React patterns')
+  })
+
+  test('The unique identifier property of the blog posts is by default _id', async () => {
+    const blogs = await Blog.find({})
+    expect(blogs[0]._id).toBeDefined()
+  })
+})
+
+describe('Addition of a new blog', () => {
+  test('A valid blog can be added ', async () => {
+    const newBlog = {
+      title:"Canonical string reduction",
+      author:"Edsger W. Dijkstra",
+      url:"http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes:12
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(n => n.title)
+    expect(contents).toContain(
+      'Canonical string reduction'
+    )
+  })
+
+  test('If title and url are missing, respond with 400 bad request', async () => {
+    const newBlog = {
+      author:"Edsger W. Dijkstra",
+      likes:12
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+
+    const blogsAtEnd = await helper.blogsInDb()
+
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
